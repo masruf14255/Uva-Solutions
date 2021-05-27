a0574383af62095601dc24b127a4bd846a417e16
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-28 01:31:30 +0600

    Create 11450.cpp

---
+++ 11450.cpp
@@ -0,0 +1,118 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+typedef unsigned int uint;
+
+typedef long long int64;
+
+typedef unsigned long long uint64;
+
+
+
+#define FOI(i, A, B) for(i=A; i<=B; i++)
+
+#define FOD(i, A, B) for(i=A; i>=B; i--)
+
+#define PI     acos(-1.0)
+
+#define INF    1<<30
+
+#define EPS    1e-9
+
+#define sqr(x) (x)*(x)
+
+
+
+int main()
+{
+
+    //freopen("testI.txt", "r", stdin);
+
+    //freopen("testO.txt", "w", stdout);
+
+    int T;
+
+    scanf("%d", &T);
+
+    while( T-- )
+    {
+
+        int M, C;
+
+        scanf("%d%d", &M, &C);
+
+        vector< vector<int> > Vec(C);
+
+        int i, m, c;
+
+        FOI(i, 0, C-1)
+        {
+
+            int K, temp;
+
+            scanf("%d", &K);
+
+            while( K-- )
+            {
+
+                scanf("%d", &temp);
+
+                Vec[i].push_back(temp);
+
+            }
+
+        }
+
+
+
+        int dp[M + 1][C + 1];
+
+        memset(dp, -1, sizeof(dp));
+
+        FOI(m, 0, M)
+        {
+
+            dp[m][0] = 0;
+
+            FOI(c, 1, C)
+            {
+
+                FOI(i, 0, Vec[c - 1].size()-1)
+                {
+
+                    int ci = Vec[c - 1][i];
+
+                    if( m >= ci && dp[m - ci][c - 1] != -1 )
+
+                        dp[m][c] = max(dp[m][c], dp[m - ci][c - 1] + ci);
+
+                }
+
+            }
+
+        }
+
+        if( dp[M][C] == -1 )
+
+            printf("no solution\n");
+
+        else
+
+            printf("%d\n", dp[M][C]);
+
+    }
+
+    //system("pause");
+
+    return 0;
+
+}
